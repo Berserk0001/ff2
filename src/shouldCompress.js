d58cfe6abebba58f7950f38c804cb0f155c0f0a2
Rushs321 <fyggh566@tutamail.com>
2024-10-23 10:53:49 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,14 +1,13 @@
 "use strict";
+const MIN_COMPRESS_LENGTH = 1024
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100
 
-const MIN_COMPRESS_LENGTH = 1024;
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
-
-function shouldCompress(request) {
-  const { originType, originSize, webp } = request.params;
+function shouldCompress(req) {
+  const { originType, originSize, webp } = req.params
 
   if (!originType.startsWith('image')) return false;
   if (originSize === 0) return false;
-  if (request.headers.range) return false;
+  if (req.headers.range) return false;
   if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
   if (
     !webp &&
@@ -21,4 +20,4 @@ function shouldCompress(request) {
   return true;
 }
 
-module.exports = shouldCompress;
+module.exports = shouldCompress
