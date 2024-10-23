9ec5974c9bff025cedd20f79896493b0d9b2951f
Rushs321 <fyggh566@tutamail.com>
2024-10-23 06:56:21 +0000

    uu

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const proxy = require('./src//proxy1')
 const params = require('./src/params')
-const PORT = process.env.PORT || 8080
 const app = express()
 
 app.get('/', params, proxy);
 
 
-app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
+ app.listen(3000, () => {
+      console.log('Server running on port 3000');
+      });
