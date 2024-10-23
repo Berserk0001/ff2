af57c64a17001425c57be23ae5096902d7d4484e
Rushs321 <fyggh566@tutamail.com>
2024-10-23 19:58:36 +0530

    Update compress3.js

--- src/compress3.js
+++ src/compress3.js
@@ -7,10 +7,14 @@
 const sharp = require('sharp');
 const redirect = require('./redirect');
 
+// Configure sharp worker concurrency and cache settings
+sharp.concurrency(1);
+sharp.cache({ memory: 256, items: 2, files: 20 });
+
 const sharpStream = () => sharp({ animated: false, unlimited: true });
 
-async function compress(req, res, input) {
-  const format = 'webp';
+function compress(req, res, input) {
+  const format = 'avif';
 
   /*
    * Determine the uncompressed image size when there's no content-length header.
@@ -24,37 +28,32 @@ async function compress(req, res, input) {
    * |x-original-size|Original photo size                |OriginSize                  |
    * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
    */
-
-  // Wrap the buffer processing in a promise to use await
-  const bufferPromise = new Promise((resolve, reject) => {
-    input.data.pipe(sharpStream()
+  
+  input.data.pipe(
+    sharpStream()
       .grayscale(req.params.grayscale)
       .toFormat(format, {
         quality: req.params.quality,
         effort: 0,
-      }))
-      .toBuffer((err, output, info) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve({ output, info });
-        }
-      });
-  });
-
-  try {
-    const { output, info } = await bufferPromise;
+      })
+  )
+  .toBuffer()
+  .then((output) => {
+    const info = sharp.info(output); // Retrieve info manually from the buffer
+    if (!info) {
+      throw new Error('Compression failed');
+    }
 
     res.setHeader('content-type', 'image/' + format);
     res.setHeader('content-length', info.size);
     res.setHeader('x-original-size', req.params.originSize);
     res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-    res.status(200);
-    res.write(output);
-    res.end();
-  } catch (err) {
-    redirect(req, res);
-  }
+    res.status(200).send(output);
+  })
+  .catch((err) => {
+    console.error('Error during compression:', err);
+    return redirect(req, res);
+  });
 }
 
 module.exports = compress;
