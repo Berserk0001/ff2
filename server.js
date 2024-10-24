eb5d660320862233c6c11b935657277b1f4b26a0
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-24 20:16:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 'use strict'
 const compression = require('compression')
 const express = require('express')
-const proxy = require('./src/proxy4')
+const proxy = require('./src/proxy3')
 const params = require('./src/params')
 const app = express()
 app.use(compression());
