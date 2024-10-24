40a530d5e424ceb9fd713767b999096e4a2122aa
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-24 14:32:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,5 @@
 #!/usr/bin/env node
 'use strict'
-const cluster = require("cluster");
-
-
-
-  const numClusters =8;
-
-  console.log(`Primary ${process.pid} is running. Will fork ${numClusters} clusters.`);
-
-  // Fork workers.
-  for (let i = 0; i < numClusters; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died. Forking another one....`);
-    cluster.fork();
-  });
-
-  
-
-
-
 const express = require('express')
 const proxy = require('./src/proxy')
 const params = require('./src/params')
@@ -31,7 +9,7 @@ app.enable('trust proxy');
 app.get('/', params, proxy);
 
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+//app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 //app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
